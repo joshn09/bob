51cef67f24e040acc09bfa8298fff765d81085a4
Joshua <taxesunpaidforthegov@gmail.com>
2024-12-10 12:46:47 +0800

    updated_handler

--- cmds/cmdList.js
+++ cmds/cmdList.js
@@ -1,7 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js')
+// const { execute } = require('./sheetdb')
 
 module.exports = {
 
+    data: new SlashCommandBuilder()
+        .setName('bobupdate')
+        .setDescription('Bob Update cmd list!'),
 
+            async execute(interaction) {
+                await interaction.reply('### Developers can finally add cmds to Bob. \n -# Updated: 12/10/2024');
+            }
 
 }
--- cmds/sheetdb.js
+++ cmds/sheetdb.js
@@ -25,7 +25,7 @@ module.exports = {
         
                                 })
         
-                                interaction.reply('### sheet api updated!');
+                                await interaction.reply('### sheet api updated!');
                             }
  }
             
