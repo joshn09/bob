16bd79e99d2c5655e49462db6f9a6f5070ef52d4
Joshua <taxesunpaidforthegov@gmail.com>
2024-12-11 19:06:54 +0800

    update

---
+++ cmds/spaceMarine.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+
+    .setName('spacemarine')
+    .setDescription('Pledge!'),
+
+        async execute(interaction) {
+          interaction.reply(` ## My honour is my life. My duty is my fate. My fear is to fail. My salvation is my reward. My craft is death. My pledge is eternal service. `);
+        }
+
+}
